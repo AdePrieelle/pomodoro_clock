855a6687b42a8bf71924344ba7b179d693f783a5
Antoine de Prieelle <antoine_@live.nl>
2020-03-25 15:59:03 +0100

    Add functionality to update the countdown timer display when the session value gets adjusted

--- script.js
+++ script.js
@@ -2,34 +2,38 @@ const timer = () => {
 
   
 
-  //values of adjustsession, adjustbreak, countdowntimer
+  //values of adjustsession and adjustbreak
   const sessionValue = document.querySelector(".adjustSessionValue");
   const breakValue = document.querySelector(".adjustBreakValue");
-  const countdownValue = document.querySelector(".countdownValue");
   //the buttons to adjust increase and decrease sessionValue and BreakValue
   const downTimerSession = document.querySelector(".downTimerSession");
   const upTimerSession = document.querySelector(".upTimerSession");
   const downTimerBreak = document.querySelector(".downTimerBreak");
   const upTimerBreak = document.querySelector(".upTimerBreak");
-  //interaction lower block will be added soon
+  //display the session or break as title and its countdown timer
+  const titleSessionOrBreak = document.querySelector(".titleSessionOrBreak");
+  const countdownValue = document.querySelector(".countdownValue");
+  //interaction buttons will be added later
 
   //adjusting the session and break times
   const adjustSessionValue = () => {
     
     downTimerSession.addEventListener("click", function() {
-      if(+sessionValue.textContent > 0) {
+      if(+sessionValue.textContent > 1) {
         sessionValue.textContent = +sessionValue.textContent - 1;
+        countdownValueDisplay();
       };
       console.log(sessionValue.textContent);
     });
 
     upTimerSession.addEventListener("click", function() {
       sessionValue.textContent = +sessionValue.textContent + 1;
+      countdownValueDisplay();
       console.log(sessionValue.textContent);
     });
 
     downTimerBreak.addEventListener("click", function() {
-      if(+breakValue.textContent > 0) {
+      if(+breakValue.textContent > 1) {
         breakValue.textContent = +breakValue.textContent - 1;
       };
       console.log(breakValue.textContent);
@@ -41,6 +45,15 @@ const timer = () => {
     });
       
   };
+
+  //Update the display for countdowntimer when the session time is adjusted
+  const countdownValueDisplay = () => {
+    countdownValue.textContent = sessionValue.textContent + ":00";
+  }
+
+  //https://www.youtube.com/watch?v=LAaf7-WuJJQ
+  //Countdown timer
+
 
 
   //call functions
